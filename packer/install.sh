edf9edc3f3f71b788d7d95d3d352ca749d229471
Ryan Smith <rms1000watt@gmail.com>
2017-11-12 09:25:20 -0800

    WIP fixing missing virtualbox/guestinfo/net

--- packer/install.sh
+++ packer/install.sh
@@ -14,10 +14,14 @@ rm /lib/udev/rules.d/75-persistent-net-generator.rules || true
 rm -rf /dev/.udev/ /var/lib/dhcp3/* || true
 echo "pre-up sleep 2" >> /etc/network/interfaces || true
 
+echo "auto eth0" >> /etc/network/interfaces || true
+echo "iface eth0 inet dhcp" >> /etc/network/interfaces || true
+
+echo "auto eth1" >> /etc/network/interfaces || true
+echo "iface eth1 inet dhcp" >> /etc/network/interfaces || true
+
+sed -i 's|GRUB_CMDLINE_LINUX=""|GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"|g' /etc/default/grub
+update-grub
+
 # Disable DNS reverse lookup
 echo "UseDNS no" >> /etc/ssh/sshd_config || true
-
-# Install and configure cloud-config
-apt-get -y install cloud-init || true
-sed -i 's/name: ubuntu/name: packer/g' /etc/cloud/cloud.cfg || true
-sed -i 's/lock_passwd: True/lock_passwd: False/g' /etc/cloud/cloud.cfg || true
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,15 @@
 resource "virtualbox_vm" "node" {
-    count = 2
+    count = 1
     name = "${format("node-%02d", count.index+1)}"
-    image = "../packer/output-virtualbox-iso/test-ubuntu-xenial.box"
+    image = "../packer/output-virtualbox-iso/test-ubuntu-xenial-5.box"
     cpus = 2
     memory = "1024mib"
-    user_data = "${file("install.sh")}"
+    # user_data = "${file("install.sh")}"
 
     network_adapter {
       type = "nat"
     }
+
     network_adapter {
         type = "bridged"
         host_interface = "en0"
