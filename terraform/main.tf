e3500d90f10df56b69b33adcdd24b020702af76d
Ryan Smith <rms1000watt@gmail.com>
2017-11-13 09:11:14 -0800

    Added terraform ssh provisioning

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,10 +4,30 @@ resource "virtualbox_vm" "node" {
     image = "../packer/output-virtualbox-iso/ubuntu-16.04-docker.box"
     cpus = 2
     memory = "1024mib"
-    # user_data = "${file("install.sh")}"
     network_adapter {
         type = "bridged"
         host_interface = "en0"
+    }
+}
+
+resource "null_resource" "node-provisioner" {
+    connection {
+        type     = "ssh"
+        host     = "${element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 1)}"
+        user     = "packer"
+        password = "packer"
+    }
+
+    provisioner "file" {
+        source      = "provision.sh"
+        destination = "/home/packer/provision.sh"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "chmod a+x /home/packer/provision.sh",
+            "/home/packer/provision.sh",
+        ]
     }
 }
 
