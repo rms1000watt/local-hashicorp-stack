0b79ff605dd866e3eff94454bdd20a1d689f9c51
Ryan Smith <rms1000watt@gmail.com>
2017-11-17 16:59:46 -0800

    WIP client consul+nomad

--- nomad/client.hcl
+++ nomad/client.hcl
@@ -4,7 +4,6 @@ datacenter = "dc-1"
 
 client {
     enabled = true
-    # servers = ["${NOMAD_SERVER_ADDRESS_1}", "${NOMAD_SERVER_ADDRESS_2}", "${NOMAD_SERVER_ADDRESS_3}"]
     options {
         "docker.privileged.enabled" = "true"
     }
--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,18 @@ resource "virtualbox_vm" "server" {
     }
 }
 
+resource "virtualbox_vm" "client" {
+    count = 1
+    name = "${format("client-%02d", count.index+1)}"
+    image = "../packer/output-virtualbox-iso/ubuntu-16.04-docker.box"
+    cpus = 2
+    memory = "1.0 gib"
+    network_adapter {
+        type = "bridged"
+        host_interface = "en0"
+    }
+}
+
 resource "null_resource" "server-provisioner" {
     # Run server-provisioner if the mac address changes (if machine is deleted not just turned off)
     triggers {
@@ -68,10 +80,73 @@ resource "null_resource" "server-provisioner" {
     }
 }
 
+resource "null_resource" "client-provisioner" {
+    # Run client-provisioner if the mac address changes (if machine is deleted not just turned off)
+    triggers {
+        mac_addresses = "${join(",", virtualbox_vm.client.*.network_adapter.0.mac_address)}"
+    }
+
+    connection {
+        type     = "ssh"
+        host     = "${element(virtualbox_vm.client.*.network_adapter.0.ipv4_address, 1)}"
+        user     = "packer"
+        password = "packer"
+        timeout  = "1m"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo mkdir /nomad",
+            "sudo chmod 777 /nomad",
+            "sudo mkdir /consul",
+            "sudo chmod 777 /consul",
+        ]
+    }
+
+    provisioner "file" {
+        source      = "../nomad/client.hcl"
+        destination = "/nomad/client.hcl"
+    }
+
+    provisioner "file" {
+        source      = "../nomad/systemd-nomad-client.service"
+        destination = "/nomad/systemd-nomad-client.service"
+    }
+
+    provisioner "file" {
+        source      = "../consul/client.json"
+        destination = "/consul/client.json"
+    }
+
+    provisioner "file" {
+        source      = "../consul/systemd-consul-client.service"
+        destination = "/consul/systemd-consul-client.service"
+    }
+
+    provisioner "file" {
+        source      = "provision.sh"
+        destination = "/home/packer/provision.sh"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo mv /nomad/systemd-nomad-client.service /etc/systemd/system/nomad.service",
+            "sudo mv /consul/systemd-consul-client.service /etc/systemd/system/consul.service",
+            "chmod a+x /home/packer/provision.sh",
+            "sudo echo -e '${element(virtualbox_vm.server.*.network_adapter.0.ipv4_address, 1)} consul-server' >> /etc/hosts",
+            "sudo /home/packer/provision.sh",
+        ]
+    }
+}
+
 output "Server Connection" {
     value = "ssh packer@${element(virtualbox_vm.server.*.network_adapter.0.ipv4_address, 1)}"
 }
 
-# output "Client Connections" {
-#     value = ["ssh packer@${virtualbox_vm.client.*.network_adapter.0.ipv4_address}"]
-# }
+output "Client Connection" {
+    value = "ssh packer@${element(virtualbox_vm.client.*.network_adapter.0.ipv4_address, 1)}"
+}
+
+output "Update /etc/hosts" {
+    value = "${element(virtualbox_vm.server.*.network_adapter.0.ipv4_address, 1)} consul-server"
+}
